93b4953cf56b5c55e74adc024fb453d77a5bf22d
iqraaslam123 <iqraaslam1966@gmail.com>
2026-07-13 21:09:48 +0500

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || (
+    import.meta.env.PROD
+      ? 'https://shop-backend-nine-ruby.vercel.app/api'
+      : '/api'
+  ),
+});
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem('userInfo'));
